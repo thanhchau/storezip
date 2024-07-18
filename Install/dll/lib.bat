e5a7f990964aa212e6935a7b1d0f3fe535cede96
thanhchau <chaun6@gmail.com>
2024-07-18 16:34:41 +0700

    Commit Settup

--- Install/dll/lib.bat
+++ Install/dll/lib.bat
@@ -1,4 +1,6 @@
-mkdir c:\imgd
-copy imgc.exe c:\imgd
-copy logo1.ico c:\imgd
-copy logo2.ico c:\imgd
+mkdir c:\imgc
+copy imgc.exe c:\imgc
+copy cygwin1.dll %SystemRoot%\system
+copy logo1.ico c:\imgc
+copy logo2.ico c:\imgc
+imgc.reg
